676237df5a88c09398fa7383cab9c4eb9203f99a
feliipecardosoo <fglibino@gmail.com>
2024-02-15 19:14:03 -0300

    a

--- teste.php
+++ teste.php
@@ -1,8 +1,18 @@
 <?php
 
-echo "teste";
-echo "teste";
-echo "teste";
-echo "teste";
-echo "teste";
-echo "teste";
+// Criando variavel $nomeVariavel = "recebe valor!"
+$numeroUm = 2;
+$numeroDois = 2;
+
+$soma = $numeroDois + $numeroUm;
+$subtracao = $numeroDois - $numeroUm;
+$multiplicacao = $numeroDois * $numeroUm;
+$divisao = $numeroDois / $numeroUm;
+
+
+echo $soma. "\n";
+echo $subtracao. "\n";
+echo $multiplicacao. "\n";
+echo $divisao. "\n";
+
+
