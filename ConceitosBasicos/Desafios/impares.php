6eedba39745867f519d8ea875be8b0c273bc2625
feliipecardosoo <fglibino@gmail.com>
2024-02-15 22:01:03 -0300

    a

---
+++ ConceitosBasicos/Desafios/impares.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($i = 0; $i <=100; $i++) {
+    if ($i % 2 == 0 ) {
+        echo "o numero $i é par!" . PHP_EOL;
+    } else {
+        echo "o numero $i é impar!" . PHP_EOL;
+    }
+}
--- ConceitosBasicos/condicionais2.php
+++ ConceitosBasicos/condicionais2.php
@@ -1,5 +1,10 @@
 <?php
 
 for ($contador = 1; $contador <= 15; $contador = $contador += 1) {
+    
+    if ($contador == 13) {
+        continue;
+    } 
     echo "#$contador" . PHP_EOL;
+
 }
